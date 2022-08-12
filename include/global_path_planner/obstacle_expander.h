8a2ca3bac63beabc2987cc136c716a1c591b6929
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-08-13 04:29:41 +0900

    fixed

--- include/global_path_planner/obstacle_expander.h
+++ include/global_path_planner/obstacle_expander.h
@@ -14,12 +14,14 @@ public:
 
 private:
     // ----- 関数 ------
-    void map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg); // コールバック関数
+    void map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg);   // コールバック関数
     void expand_obstacle(); // 障害物の膨張
+    void change_surrounding_grid_color(const int occupied_grid_index); // 周囲のグリッドの色の変更
+    bool in_same_line(const int occupied_grid_index, const int index); // 占有グリッドと同じ行内か判断
 
 
     // ----- 変数 -----
-    int hz_;         // ループ周波数 [Hz]
+    int    hz_;            // ループ周波数 [Hz]
     double target_margin_; // 車両マージン [m]
 
     // msg受け取りフラッグ
@@ -32,13 +34,13 @@ private:
     ros::NodeHandle private_nh_;
 
     // Subscriber
-    ros::Subscriber sub_original_map_;
+    ros::Subscriber sub_raw_map_;
 
     // Publisher
     ros::Publisher pub_updated_map_;
 
     // 各種オブジェクト
-    nav_msgs::OccupancyGrid original_map_; // map_serverから受け取るマップ
+    nav_msgs::OccupancyGrid raw_map_;     // map_serverから受け取るマップ
     nav_msgs::OccupancyGrid updated_map_; // 障害物を車両マージン分膨張させたマップ
 };
 
--- src/obstacle_expander.cpp
+++ src/obstacle_expander.cpp
@@ -11,7 +11,7 @@ ObstacleExpander::ObstacleExpander():private_nh_("~")
     updated_map_.header.frame_id = "map";
 
     // Subscriber
-    sub_original_map_ = nh_.subscribe("/map", 1, &ObstacleExpander::map_callback, this);
+    sub_raw_map_ = nh_.subscribe("/map", 1, &ObstacleExpander::map_callback, this);
 
     // Publisher
     pub_updated_map_ = nh_.advertise<nav_msgs::OccupancyGrid>("/map/updated_map", 1);
@@ -20,7 +20,7 @@ ObstacleExpander::ObstacleExpander():private_nh_("~")
 // mapのコールバック関数
 void ObstacleExpander::map_callback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-    original_map_ = *msg;
+    raw_map_  = *msg;
     flag_map_ = true;
 }
 
@@ -38,11 +38,84 @@ void ObstacleExpander::process()
     }
 }
 
-// 障害物の膨張
+// 障害物を膨張
 void ObstacleExpander::expand_obstacle()
 {
-    updated_map_ = original_map_;
+    updated_map_ = raw_map_;
 
+    const int size = raw_map_.data.size();
+    for(int index=0; index<size; index++)
+        if(raw_map_.data[index] == 100) // 「占有」のとき
+            change_surrounding_grid_color(index); // 周囲のグリッドの色の変更
 
     pub_updated_map_.publish(updated_map_);
+}
+
+// 周囲のグリッドの色を変更
+void ObstacleExpander::change_surrounding_grid_color(const int occupied_grid_index)
+{
+    const int target_margin_cell = int(round(target_margin_/updated_map_.info.resolution)); // 車両マージン [cell]
+
+    // 上下に膨張
+    for(int i=0; i<2; i++)
+    {
+        int index = occupied_grid_index;
+        for(int j=0; j<target_margin_cell; j++)
+        {
+            switch(i)
+            {
+                case 0 : // 上方向に膨張
+                    index -= updated_map_.info.width;
+                    break;
+                case 1 : // 下方向に膨張
+                    index += updated_map_.info.width;
+                    break;
+            }
+
+            // 配列dataの範囲内のとき
+            if(0 <= index && index < updated_map_.data.size())
+                updated_map_.data[index] = 100;
+        }
+    }
+
+    // 左右に膨張
+    for(int i=0; i<2; i++)
+    {
+        int index = occupied_grid_index;
+        for(int j=0; j<target_margin_cell; j++)
+        {
+            switch(i)
+            {
+                case 0 : // 左方向に膨張
+                    index--;
+                    break;
+                case 1 : // 右方向に膨張
+                    index++;
+                    break;
+            }
+
+            // 配列dataの範囲内かつ占有グリッドと同じ行内のとき
+            if((0 <= index && index < updated_map_.data.size()) && in_same_line(occupied_grid_index, index))
+                updated_map_.data[index] = 100;
+        }
+    }
+}
+
+// 占有グリッドと同じ行内の場合、trueを返す
+bool ObstacleExpander::in_same_line(const int occupied_grid_index, const int index)
+{
+    // 占有グリッドを含む行のインデックスの最大値・最小値の算出
+    int min_index = 0;
+    int max_index = updated_map_.info.width-1;
+    while(max_index < occupied_grid_index)
+    {
+        min_index += updated_map_.info.width;
+        max_index += updated_map_.info.width;
+    }
+
+    // 同じ行内か判断
+    if(min_index <= index && index <= max_index)
+        return true;
+    else
+        return false;
 }
