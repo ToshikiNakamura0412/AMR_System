ea3851dd97dd017b593d4075934fb0d461c49f26
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-08-20 23:50:05 +0900

    reduced mode

--- src/obstacle_expander.cpp
+++ src/obstacle_expander.cpp
@@ -1,9 +1,3 @@
-/*
-二つの膨張方法を実装
-    mode1: 上下左右に膨張(実行時間: 2.2s)
-    mode2: 円形状に膨張  (実行時間: 0.3s)
-*/
-
 #include "global_path_planner/obstacle_expander.h"
 
 // コンストラクタ
@@ -11,7 +5,6 @@ ObstacleExpander::ObstacleExpander():private_nh_("~")
 {
     // パラメータの取得
     private_nh_.getParam("hz", hz_);
-    private_nh_.getParam("mode", mode_);
     private_nh_.getParam("target_margin", target_margin_);
 
     // Subscriber
@@ -51,118 +44,15 @@ void ObstacleExpander::expand_obstacle()
 
     const int size = raw_map_.data.size();
     for(int index=0; index<size; index++)
-    {
-        // 「占有」のとき
-        if(raw_map_.data[index] == 100)
-        {
-            // 周囲のグリッドの色の変更
-            if(mode_ == 1) change_surrounding_grid_color1(index); // mode1:上下左右に膨張
-            if(mode_ == 2) change_surrounding_grid_color2(index); // mode2:円形状に膨張
-        }
-    }
+        if(raw_map_.data[index] == 100) // 「占有」のとき
+            change_surrounding_grid_color(index); // 周囲のグリッドの色の変更
 
     pub_updated_map_.publish(updated_map_);
     show_exe_time() // 実行時間を表示
 }
 
-
-// ========== mode1 ==========
-// 周囲のグリッドの色を変更（上下左右に膨張）
-void ObstacleExpander::change_surrounding_grid_color1(const int occupied_grid_index)
-{
-    const int target_margin_cell = int(round(target_margin_/updated_map_.info.resolution)); // 車両マージン [cell]
-
-    // 上下に膨張
-    for(int i=0; i<2; i++)
-    {
-        int index = occupied_grid_index;
-        for(int j=0; j<target_margin_cell; j++)
-        {
-            switch(i)
-            {
-                case 0 : // 上方向に膨張
-                    index -= updated_map_.info.width;
-                    break;
-                case 1 : // 下方向に膨張
-                    index += updated_map_.info.width;
-                    break;
-            }
-
-            // 配列dataの範囲内のとき
-            if(0 <= index && index < updated_map_.data.size())
-                updated_map_.data[index] = 100;
-        }
-    }
-
-    // 左右に膨張
-    for(int i=0; i<2; i++)
-    {
-        int index = occupied_grid_index;
-        for(int j=0; j<target_margin_cell; j++)
-        {
-            switch(i)
-            {
-                case 0 : // 左方向に膨張
-                    index--;
-                    break;
-                case 1 : // 右方向に膨張
-                    index++;
-                    break;
-            }
-
-            // 配列dataの範囲内かつ占有グリッドと同じ行内のとき
-            if((0 <= index && index < updated_map_.data.size()) && in_same_line(occupied_grid_index, index))
-                updated_map_.data[index] = 100;
-        }
-    }
-}
-
-// 占有グリッドと同じ行内の場合、trueを返す
-bool ObstacleExpander::in_same_line(const int occupied_grid_index, const int index)
-{
-    // 占有グリッドを含む行のインデックスの最大値・最小値の算出
-    int max_index = get_max_grid_index_in_same_line(occupied_grid_index);
-    int min_index = get_mix_grid_index_in_same_line(occupied_grid_index);
-
-    // 同じ行内か判断
-    if(min_index <= index && index <= max_index)
-        return true;
-    else
-        return false;
-}
-
-// 占有グリッドを含む行のインデックスの最大値の算出
-bool ObstacleExpander::get_max_grid_index_in_same_line(const int index)
-{
-    int min_index = 0;
-    int max_index = updated_map_.info.width-1;
-    while(max_index < index)
-    {
-        min_index += updated_map_.info.width;
-        max_index += updated_map_.info.width;
-    }
-
-    return max_index;
-}
-
-// 占有グリッドを含む行のインデックスの最小値の算出
-bool ObstacleExpander::get_mix_grid_index_in_same_line(const int index)
-{
-    int min_index = 0;
-    int max_index = updated_map_.info.width-1;
-    while(max_index < index)
-    {
-        min_index += updated_map_.info.width;
-        max_index += updated_map_.info.width;
-    }
-
-    return min_index;
-}
-
-
-// ========== mode2 ==========
 // 周囲のグリッドの色を変更（円形状に膨張）
-void ObstacleExpander::change_surrounding_grid_color2(const int occupied_grid_index)
+void ObstacleExpander::change_surrounding_grid_color(const int occupied_grid_index)
 {
     search_rect_grid_index_list(occupied_grid_index); // 探索対象のグリッドのインデックスを追加
     const int size = rect_grid_index_list_.size();
@@ -237,6 +127,32 @@ int ObstacleExpander::get_upper_left_grid_index(const int center_grid_index)
     return index;
 }
 
+// 占有グリッドを含む行のインデックスの最小値の算出
+bool ObstacleExpander::get_mix_grid_index_in_same_line(const int index)
+{
+    int min_index = 0;
+    int max_index = updated_map_.info.width-1;
+    while(max_index < index)
+    {
+        min_index += updated_map_.info.width;
+        max_index += updated_map_.info.width;
+    }
+
+    return min_index;
+}
+
+// グリッドのインデックスからxのインデックスを計算
+int ObstacleExpander::get_grid_index_x(const int index)
+{
+    return index % updated_map_.info.width;
+}
+
+// グリッドのインデックスからyのインデックスを計算
+int ObstacleExpander::get_grid_index_y(const int index)
+{
+    return index / updated_map_.info.width;
+}
+
 // rect_grid_index_listの右下のインデックスを検索
 int ObstacleExpander::get_lower_right_grid_index(const int center_grid_index)
 {
@@ -269,16 +185,18 @@ int ObstacleExpander::get_lower_right_grid_index(const int center_grid_index)
     return index;
 }
 
-// グリッドのインデックスからxのインデックスを計算
-int ObstacleExpander::get_grid_index_x(const int index)
+// 占有グリッドを含む行のインデックスの最大値の算出
+bool ObstacleExpander::get_max_grid_index_in_same_line(const int index)
 {
-    return index % updated_map_.info.width;
-}
+    int min_index = 0;
+    int max_index = updated_map_.info.width-1;
+    while(max_index < index)
+    {
+        min_index += updated_map_.info.width;
+        max_index += updated_map_.info.width;
+    }
 
-// グリッドのインデックスからyのインデックスを計算
-int ObstacleExpander::get_grid_index_y(const int index)
-{
-    return index / updated_map_.info.width;
+    return max_index;
 }
 
 // グリッド同士の距離[cell]を計算
