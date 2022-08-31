0f1112c048483dfa18cb654a0a3d487ac5eb5103
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-09-01 05:09:43 +0900

    fixed

--- src/emcl.cpp
+++ src/emcl.cpp
@@ -2,9 +2,8 @@
 emcl: mcl with expansion resetting
 */
 
-#include "localizer/localizer.h"
+#include "localizer/emcl.h"
 
-// ----- EMCL -----
 // コンストラクタ
 EMCL::EMCL():private_nh_("~"), engine_(seed_gen_())
 {
@@ -587,49 +586,4 @@ void EMCL::publish_particles()
 
         pub_particle_cloud_.publish(particle_cloud_);
     }
-}
-
-
-
-// ----- OdomModel -----
-// コンストラクタ
-OdomModel::OdomModel(const double ff, const double fr, const double rf, const double rr)
-    : engine_(seed_gen_()), std_norm_dist_(0.0, 1.0), fw_dev_(0.0), rot_dev_(0.0)
-{
-    fw_var_per_fw_   = pow(ff ,2.0);
-    fw_var_per_rot_  = pow(fr ,2.0);
-    rot_var_per_fw_  = pow(rf ,2.0);
-    rot_var_per_rot_ = pow(rr ,2.0);
-}
-
-// 並進，回転に関する標準偏差の設定
-void OdomModel::set_dev(const double length, const double angle)
-{
-    fw_dev_  = sqrt(fabs(length)*fw_var_per_fw_  + fabs(angle)*fw_var_per_rot_);
-    rot_dev_ = sqrt(fabs(length)*rot_var_per_fw_ + fabs(angle)*rot_var_per_rot_);
-}
-
-// 直進に関するノイズの取得
-double OdomModel::get_fw_noise()
-{
-    return std_norm_dist_(engine_) * fw_dev_;
-}
-
-// 回転に関するノイズの取得
-double OdomModel::get_rot_noise()
-{
-    return std_norm_dist_(engine_) * rot_dev_;
-}
-
-// 代入演算子
-OdomModel& OdomModel::operator =(const OdomModel& t)
-{
-    fw_var_per_fw_   = t.fw_var_per_fw_;
-    fw_var_per_rot_  = t.fw_var_per_rot_;
-    rot_var_per_fw_  = t.rot_var_per_fw_;
-    rot_var_per_rot_ = t.rot_var_per_rot_;
-    fw_dev_  = t.fw_dev_;
-    rot_dev_ = t.rot_dev_;
-
-    return *this;
 }
