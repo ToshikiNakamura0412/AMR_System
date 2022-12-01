2237a40e7f09a1f7983276b0f7750b2fbf76aafc
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-12-01 21:29:29 +0900

    Fix

--- src/image_hz_checker.cpp
+++ src/image_hz_checker.cpp
@@ -28,25 +28,32 @@ private:
 // コンストラクタ
 HzChecker::HzChecker():private_nh_("~")
 {
-    private_nh_.param("hz", hz_, 10);
-    sub_something_ = nh_.subscribe("topic_name", 1, &HzChecker::something_callback, this);
+    private_nh_.param("hz", hz_, 100);
+    private_nh_.param("count", count_, 0);
+    sub_something_ = nh_.subscribe("/topic_name", 1, &HzChecker::something_callback, this);
 }
 
 
 // ===== 関数 =====
 void HzChecker::something_callback(const sensor_msgs::CompressedImage::ConstPtr& msg)
 {
-    ROS_INFO_STREAM("got!!");
+    count_++;
+    if(count_ == 50)
+    {
+        ROS_INFO_STREAM(std::fixed << std::setprecision(3) << 50.0/(ros::Time::now().toSec()-begin_.toSec()) << " Hz");
+        begin_ = ros::Time::now();
+        count_ = 0;
+    }
 }
 
 void HzChecker::process()
 {
-
     ros::Rate loop_rate(hz_);
+    begin_ = ros::Time::now();
     while(ros::ok())
     {
         ros::spinOnce();
-        // loop_rate.sleep();
+        loop_rate.sleep();
     }
 }
 
@@ -54,7 +61,7 @@ void HzChecker::process()
 // ===== メイン関数 =====
 int main(int argc, char* argv[])
 {
-    ros::init(argc, argv, "hz_checker");
+    ros::init(argc, argv, "image_hz_checker");
     HzChecker hz_checker;
     hz_checker.process();
     return 0;
