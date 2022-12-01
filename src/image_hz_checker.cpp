79edd1998c05074cd57c1356621efa66a3e13aa3
Toshiki Nakamura <ee193014@meiji.ac.jp>
2022-12-02 02:25:29 +0900

    Fix

--- src/image_hz_checker.cpp
+++ src/image_hz_checker.cpp
@@ -13,9 +13,12 @@ private:
         void something_callback(const sensor_msgs::CompressedImage::ConstPtr& msg);
 
         // ----- 変数 -----
-        int hz_;
-        int count_;
+        int  hz_;
+        int  count_;
+        int  count_max_;
+        bool is_visible_;
         ros::Time begin_;
+
 
         // ----- その他オブジェクト -----
         // NodeHandle
@@ -30,6 +33,8 @@ HzChecker::HzChecker():private_nh_("~")
 {
     private_nh_.param("hz", hz_, 100);
     private_nh_.param("count", count_, 0);
+    private_nh_.param("count_max", count_max_, 50);
+    private_nh_.param("is_visible", is_visible_, false);
     sub_something_ = nh_.subscribe("/topic_name", 1, &HzChecker::something_callback, this);
 }
 
@@ -38,9 +43,13 @@ HzChecker::HzChecker():private_nh_("~")
 void HzChecker::something_callback(const sensor_msgs::CompressedImage::ConstPtr& msg)
 {
     count_++;
-    if(count_ == 50)
+    if(count_ == count_max_)
     {
-        ROS_INFO_STREAM(std::fixed << std::setprecision(3) << 50.0/(ros::Time::now().toSec()-begin_.toSec()) << " Hz");
+        if(is_visible_)
+            ROS_INFO_STREAM("[" << ros::this_node::getName().c_str() << "] " << std::fixed << std::setprecision(3) << 50.0/(ros::Time::now().toSec()-begin_.toSec()) << " Hz");
+        else
+            ROS_INFO_STREAM(std::fixed << std::setprecision(3) << count_max_/(ros::Time::now().toSec()-begin_.toSec()) << " Hz");
+
         begin_ = ros::Time::now();
         count_ = 0;
     }
